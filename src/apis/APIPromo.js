87ae45ef8dfc99991970452e8a94c7140596d833
almiramaharani <almira.eka@ui.ac.id>
2023-11-15 18:33:25 +0700

    028 - Fetch API Update promo

--- src/apis/APIPromo.js
+++ src/apis/APIPromo.js
@@ -5,7 +5,13 @@ import { axiosInstance } from '@/configs/axiosInstance';
 export const APIPromo = {
     editPromo: async (id, data) => {
         try {
-            const result = await axiosInstance.put(`/admin/promo/${id}`, data);
+            const formData = new FormData();
+
+            Object.entries(data).forEach(([key, value]) => {
+                formData.append(key, key === 'status_aktif' ? value === 'true' : value);
+            });
+            
+            const result = await axiosInstance.put(`/admin/promo/${id}`, formData);
             toast.success(result.data.message);
         } catch (error) {
             toast.error(error.response.data.message);
--- src/features/promo/components/EditPromo.jsx
+++ src/features/promo/components/EditPromo.jsx
@@ -4,18 +4,21 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as y from 'yup';
 
 import { APIPromo } from "@/apis/APIPromo";
-import { DropdownField, InputField } from "@/components/Forms";
+import { DropdownField, InputField, TextareaField } from "@/components/Forms";
 import { Button } from "@/components/ui/button";
+import { Spinner } from '@/components/Elements';
 import { formatDate } from "@/utils/format";
 
 const schema = y.object({
     title: y.string().required('Judul promo tidak boleh kosong!'),
     nama_promo: y.string().required('Nama promo tidak boleh kosong!'),
     kode_voucher: y.string().required('Kode promo tidak boleh kosong!'),
-    jumlah_potongan_persen: y.string().required('Diskon promo tidak boleh kosong!').test('is-number', 'Diskon promo harus berupa angka', (value) => {
-        return !isNaN(value);
-    }),
+    jumlah_potongan_persen: y.string().required('Diskon promo tidak boleh kosong!')
+        .test('is-number', 'Diskon promo harus berupa angka', (value) => {
+            return !isNaN(value);
+        }),
     status_aktif: y.string().required('Status promo harus diisi!'),
+    deskripsi: y.string().required("Deskripsi promo tidak boleh kosong!"),
 });
 
 const statusOptions = [
@@ -25,6 +28,7 @@ const statusOptions = [
 
 export const EditPromo = ({id}) => {
     const [ promo, setPromo ] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
 
     useEffect(() => {
@@ -43,9 +47,14 @@ export const EditPromo = ({id}) => {
 
     const onSubmit = async (data) => {
         try {
-            console.log(data);
+            setIsLoading(true);
+            await APIPromo.editPromo(id, data);
+            // back to promo page
         } catch (error) {
             console.log(error);
+            setIsLoading(false);
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -65,73 +74,91 @@ export const EditPromo = ({id}) => {
 
     return (
         <div className="mt-8 overflow-hidden rounded-lg bg-white shadow p-10">
-            <form onSubmit={handleSubmit(onSubmit)} id='editPromo' className="space-y-4">
-                <div className="grid grid-rows-4 grid-flow-col gap-4">
-                    {/* Judul Promo */}
-                    <InputField
-                        isRequired
-                        placeholder="Masukkan judul promo"
-                        label="Judul Promo"
-                        autoComplete="off"
-                        registration={register('title')}
-                        error={errors.title}
-                    />
+            <form onSubmit={handleSubmit(onSubmit)} id='editPromo' 
+            className="space-y-5"
+            >
+                <div className="flex flex-col md:flex-row gap-4">
+                    <div className="flex flex-col md:flex-grow gap-4">
+                        {/* Judul Promo */}
+                        <InputField
+                            isRequired
+                            placeholder="Masukkan judul promo"
+                            label="Judul Promo"
+                            autoComplete="off"
+                            registration={register('title')}
+                            error={errors.title}
+                        />
 
-                    {/* Kode Promo */}
-                    <InputField
-                        isRequired
-                        placeholder="Masukkan kode promo"
-                        label="Kode Promo"
-                        autoComplete="off"
-                        registration={register('kode_voucher')}
-                        error={errors.kode_voucher}
-                    />
+                        {/* Kode Promo */}
+                        <InputField
+                            isRequired
+                            placeholder="Masukkan kode promo"
+                            label="Kode Promo"
+                            autoComplete="off"
+                            registration={register('kode_voucher')}
+                            error={errors.kode_voucher}
+                        />
 
-                    {/* Diskon Promo */}
-                    <InputField
-                        isRequired
-                        placeholder="Masukkan diskon promo"
-                        label="Diskon (Masukkan Angka)"
-                        autoComplete="off"
-                        registration={register('jumlah_potongan_persen')}
-                        error={errors.jumlah_potongan_persen}
-                    />
+                        {/* Diskon Promo */}
+                        <InputField
+                            isRequired
+                            placeholder="Masukkan diskon promo"
+                            label="Diskon (Masukkan Angka)"
+                            autoComplete="off"
+                            registration={register('jumlah_potongan_persen')}
+                            error={errors.jumlah_potongan_persen}
+                        />
 
-                    {/* Status */}
-                    <DropdownField
-                        isRequired
-                        label='Status'
-                        options={statusOptions}
-                        registration={register('status_aktif')}
-                        error={errors.status_aktif}
-                    />
+                        {/* Status */}
+                        <DropdownField
+                            isRequired
+                            label='Status'
+                            options={statusOptions}
+                            registration={register('status_aktif')}
+                            error={errors.status_aktif}
+                        />
 
-                    {/* Deskripsi */}
-                    {/* Peraturan */}
-                    {/* Gambar Promo */}
+                        {/* Deskripsi */}
+                        <TextareaField
+                            isRequired
+                            label='Deskripsi'
+                            placeholder='Masukkan deskripsi promo'
+                            autoComplete="off"
+                            registration={register('deskripsi')}
+                            error={errors.deskripsi}
+                            className='row-span-2'
+                        />
 
-                    {/* Nama Promo */}
-                    <InputField
-                        isRequired
-                        placeholder="Masukkan nama promo"
-                        label="Nama Promo"
-                        autoComplete="off"
-                        registration={register('nama_promo')}
-                        error={errors.nama_promo}
-                    />
+                        {/* Peraturan */}
 
-                    {/* Tanggal Kadaluarsa */}
-                    <InputField
-                        type="date"
-                        isRequired
-                        placeholder="Pilih tanggal"
-                        label="Tanggal Kadaluarsa"
-                        autoComplete="off"
-                        registration={register('tanggal_kadaluarsa')}
-                        error={errors.tanggal_kadaluarsa}
-                    />
+                        {/* Gambar Promo */}
+
+                    </div>
+
+                    <div className="flex flex-col md:flex-grow gap-4">
+                        {/* Nama Promo */}
+                        <InputField
+                            isRequired
+                            placeholder="Masukkan nama promo"
+                            label="Nama Promo"
+                            autoComplete="off"
+                            registration={register('nama_promo')}
+                            error={errors.nama_promo}
+                        />
+
+                        {/* Tanggal Kadaluarsa */}
+                        <InputField
+                            type="date"
+                            isRequired
+                            placeholder="Pilih tanggal"
+                            label="Tanggal Kadaluarsa"
+                            autoComplete="off"
+                            registration={register('tanggal_kadaluarsa')}
+                            error={errors.tanggal_kadaluarsa}
+                        />
+                    </div>
                 </div>
-                <div className="grid grid-rows-1 justify-items-end">
+                <div className="grid grid-rows-1 md:justify-items-end justify-items-center">
                     <div>
                         <Button 
                             type='button'
@@ -140,8 +167,8 @@ export const EditPromo = ({id}) => {
                             >
                             Batal
                         </Button>
-                        <Button form='editPromo' type='submit'>
-                            Simpan
+                        <Button form='editPromo' type='submit' disabled={isLoading}>
+                            {isLoading && <Spinner size="sm" className="mr-3" />} Simpan
                         </Button>
                     </div>
                 </div>
