45c0c37de52e686a6aea4ba1aeb73ba77945e507
ganangadjie <ganangaji221@gmail.com>
2023-11-24 15:33:59 +0700

    Fixing : Tampilan Tambah Promo

--- src/features/promo/routes/index.jsx
+++ src/features/promo/routes/index.jsx
@@ -62,20 +62,20 @@ export const AddPromoRoute = () => {
 
   return (
     <>
-      <ContentLayout title="Add Promo">
+      <ContentLayout title="Tambah Promo">
         <div className="justify-between sm:flex">
           <Breadcrumb>
             <Link to="/promo">
               <span className="px-2 text-sm font-semibold text-gray-700">Promo</span>
             </Link>
-            <span className="px-2 text-sm font-semibold text-primary-100">Add Promo</span>
+            <span className="px-2 text-sm font-semibold text-primary-100">Tambah Promo</span>
           </Breadcrumb>
         </div>
 
         <div className="mt-6">
           <div className="mb-8 flex items-center justify-between">
             <div className="sm:flex-auto">
-              <h1 className="text-xl font-semibold text-gray-900 sm:text-3xl">Add Promo</h1>
+              <h1 className="text-xl font-semibold text-gray-900 sm:text-3xl">Tambah Promo</h1>
             </div>
           </div>
         </div>
